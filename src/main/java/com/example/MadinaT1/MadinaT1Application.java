6024fdecb3119b1503fae8bd22a410b73afe4374
Madinusha <madinamalika19@yandex.ru>
2025-06-05 21:13:33 +0300

    initial commit

---
+++ src/main/java/com/example/MadinaT1/MadinaT1Application.java
@@ -0,0 +1,13 @@
+package com.example.MadinaT1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadinaT1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadinaT1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MadinaT1/MadinaT1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MadinaT1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadinaT1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
